5e0620b2abe91a8e77698b8e09af5c49d08b6bcc
aghvangasparyan21 <124164080+aghvangasparyan21@users.noreply.github.com>
2023-02-06 02:09:39 +0400

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package Running;
-
-import java.util.*;
-    public class Main {
-        public static void main(String[] args) {
-        Controller tmp = new Controller();
-        tmp.validLenght();
-        }
-    }
