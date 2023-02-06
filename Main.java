f55b9e2b8674d83d6ae1f130d4088df1059183cc
aghvangasparyan21 <124164080+aghvangasparyan21@users.noreply.github.com>
2023-02-06 23:00:21 +0400

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package BitVector;
-public class Main {
-    public static void main(String[] args) {
-        Controller control = new Controller();
-        int[] memory = control.validVector();
-        control.mainProcess(memory);
-    }
-}
